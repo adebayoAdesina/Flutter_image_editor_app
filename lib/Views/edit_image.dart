4ac151d24a4a4afb720ee2e425a69127f30aa49d
adebayoAdesina <adebayoadesina789@gmail.com>
2022-05-27 13:26:34 +0100

    Navbar item fixed

--- lib/Views/edit_image.dart
+++ lib/Views/edit_image.dart
@@ -17,52 +17,53 @@ class _EditImageState extends EditImageViewModel {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: _appBar,
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.2,
-            ),
-            Stack(children: [
-              _selectedImage,
-              for (var i = 0; i < info.length; i++)
-                Positioned(
-                  left: info[i].left,
-                  top: info[i].top,
-                  child: GestureDetector(
-                    child: Draggable(
-                      child: ImageText(textInfo: info[i]),
-                      feedback: ImageText(textInfo: info[i]),
-                      onDragEnd: (drag) {
-                        final renderBox =
-                            context.findRenderObject() as RenderBox;
-                        Offset off = renderBox.globalToLocal(drag.offset);
-                        setState(() {
-                          info[i].top = off.dy - 96;
-                          info[i].left = off.dx;
-                        });
+        child: SingleChildScrollView(
+          physics: const ScrollPhysics(),
+          child: Column(
+            children: [
+              Stack(children: [
+                _selectedImage,
+                for (var i = 0; i < info.length; i++)
+                  Positioned(
+                    left: info[i].left,
+                    top: info[i].top,
+                    child: GestureDetector(
+                      child: Draggable(
+                        child: ImageText(textInfo: info[i]),
+                        feedback: ImageText(textInfo: info[i]),
+                        onDragEnd: (drag) {
+                          final renderBox =
+                              context.findRenderObject() as RenderBox;
+                          Offset off = renderBox.globalToLocal(drag.offset);
+                          setState(() {
+                            info[i].top = off.dy - 96;
+                            info[i].left = off.dx;
+                          });
+                        },
+                      ),
+                      onLongPress: () {
+                        removeText(context, i);
                       },
+                      onTap: () => setCurrentIndex(context, i),
                     ),
-                    onLongPress: () {
-                      print('long press detected');
-                    },
-                    onTap: () {
-                      print('single press detected');
-                    },
                   ),
-                ),
-              creatorText.text.isNotEmpty
-                  ? Positioned(
-                      child: Text(
-                      creatorText.text,
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black.withOpacity(0.3)),
-                    ))
-                  : const SizedBox.shrink()
-            ])
-          ],
+                creatorText.text.isNotEmpty
+                    ? Positioned(
+                        left: 0,
+                        bottom: 0,
+                        child: Text(
+                          creatorText.text,
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black.withOpacity(0.3)),
+                        ))
+                    : const SizedBox.shrink()
+              ])
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -80,5 +81,160 @@ class _EditImageState extends EditImageViewModel {
           width: double.infinity,
           fit: BoxFit.fill,
         ),
+      );
+
+  AppBar get _appBar => AppBar(
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        title: SizedBox(
+            height: 50,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.save,
+                    color: Colors.black,
+                  ),
+                  tooltip: 'Save Image',
+                ),
+                IconButton(
+                  onPressed: () => increaseFontSize(),
+                  icon: const Icon(
+                    Icons.add,
+                    color: Colors.black,
+                  ),
+                  tooltip: 'Add font size',
+                ),
+                IconButton(
+                  onPressed: () => decreaseFontSize(),
+                  icon: const Icon(
+                    Icons.remove,
+                    color: Colors.black,
+                  ),
+                  tooltip: 'Decrease font size',
+                ),
+                IconButton(
+                  onPressed: () => alignFont('left'),
+                  icon: const Icon(
+                    Icons.format_align_left,
+                    color: Colors.black,
+                  ),
+                  tooltip: 'Align Left',
+                ),
+                IconButton(
+                  onPressed: () => alignFont('center'),
+                  icon: const Icon(
+                    Icons.format_align_center,
+                    color: Colors.black,
+                  ),
+                  tooltip: 'Align Center',
+                ),
+                IconButton(
+                  onPressed: () => alignFont('right'),
+                  icon: const Icon(
+                    Icons.format_align_right,
+                    color: Colors.black,
+                  ),
+                  tooltip: 'Align Right',
+                ),
+                IconButton(
+                  onPressed: () => boldFontSize(),
+                  icon: const Icon(
+                    Icons.format_bold,
+                    color: Colors.black,
+                  ),
+                  tooltip: 'Bold',
+                ),
+                IconButton(
+                  onPressed: italicText,
+                  icon: const Icon(
+                    Icons.format_italic,
+                    color: Colors.black,
+                  ),
+                  tooltip: 'Italic',
+                ),
+                IconButton(
+                  onPressed: () => addLinesToText(),
+                  icon: const Icon(
+                    Icons.space_bar,
+                    color: Colors.black,
+                  ),
+                  tooltip: 'Add New Line',
+                ),
+                Tooltip(
+                  message: 'white',
+                  child: GestureDetector(
+                    onTap: () => changeTextColor(Colors.grey.withOpacity(0.2)),
+                    child: CircleAvatar(
+                        backgroundColor: Colors.grey.withOpacity(0.2)),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Tooltip(
+                  message: 'green',
+                  child: GestureDetector(
+                    onTap: () => changeTextColor(Colors.green),
+                    child: const CircleAvatar(backgroundColor: Colors.green),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Tooltip(
+                  message: 'blue',
+                  child: GestureDetector(
+                    onTap: () => changeTextColor(Colors.blue),
+                    child: const CircleAvatar(backgroundColor: Colors.blue),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Tooltip(
+                  message: 'red',
+                  child: GestureDetector(
+                    onTap: () => changeTextColor(Colors.red),
+                    child: const CircleAvatar(backgroundColor: Colors.red),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Tooltip(
+                  message: 'black',
+                  child: GestureDetector(
+                    onTap: () => changeTextColor(Colors.black),
+                    child: const CircleAvatar(backgroundColor: Colors.black),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Tooltip(
+                  message: 'Orange',
+                  child: GestureDetector(
+                    onTap: () => changeTextColor(Colors.orange),
+                    child: const CircleAvatar(backgroundColor: Colors.orange),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Tooltip(
+                  message: 'pink',
+                  child: GestureDetector(
+                    onTap: () => changeTextColor(Colors.pink),
+                    child: const CircleAvatar(backgroundColor: Colors.pink),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+              ],
+            )),
       );
 }
--- lib/Widgets/edit_image_viewmode.dart
+++ lib/Widgets/edit_image_viewmode.dart
@@ -7,6 +7,7 @@ abstract class EditImageViewModel extends State<EditImage> {
   final TextEditingController textEditingController = TextEditingController();
   final TextEditingController creatorText = TextEditingController();
   List<TextInfo> info = [];
+  int currentIndex = 0;
 
   addNewText(BuildContext context) {
     setState(() {
@@ -50,5 +51,109 @@ abstract class EditImageViewModel extends State<EditImage> {
         ],
       ),
     );
+  }
+
+  setCurrentIndex(BuildContext context, int i) {
+    setState(() {
+      currentIndex = i;
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text(
+      'Selected for styling',
+      style: TextStyle(fontSize: 16.0),
+    )));
+  }
+
+  changeTextColor(Color colors) {
+    setState(() {
+      info[currentIndex].color = colors;
+    });
+  }
+
+  increaseFontSize() {
+    setState(() {
+      info[currentIndex].fontSize += 2;
+    });
+  }
+
+  decreaseFontSize() {
+    setState(() {
+      info[currentIndex].fontSize -= 2;
+    });
+  }
+
+  addLinesToText() {
+    setState(() {
+      if (info[currentIndex].text.contains('\n')) {
+        info[currentIndex].text = info[currentIndex].text.replaceAll('\n', '');
+      } else {
+        info[currentIndex].text = info[currentIndex].text.replaceAll(' ', '\n');
+      }
+    });
+  }
+
+  FontWeight? fontWeight;
+
+  boldFontSize() {
+    if (fontWeight == FontWeight.bold) {
+      setState(() {
+        info[currentIndex].fontweight = fontWeight!;
+        fontWeight = FontWeight.normal;
+      });
+    } else {
+      setState(() {
+        info[currentIndex].fontweight = fontWeight!;
+        fontWeight = FontWeight.bold;
+      });
+    }
+  }
+
+  FontStyle fontStyle = FontStyle.italic;
+
+  italicText() {
+    if (fontStyle == FontStyle.italic) {
+      setState(() {
+        info[currentIndex].fontStyle = fontStyle;
+        fontStyle = FontStyle.normal;
+      });
+    } else {
+      setState(() {
+        info[currentIndex].fontStyle = fontStyle;
+        fontStyle = FontStyle.italic;
+      });
+    }
+  }
+
+  alignFont(String value) {
+    switch (value) {
+      case 'left':
+        setState(() {
+          info[currentIndex].textAlign = TextAlign.left;
+        });
+        break;
+      case 'right':
+        setState(() {
+          info[currentIndex].textAlign = TextAlign.right;
+        });
+        break;
+      case 'center':
+        setState(() {
+          info[currentIndex].textAlign = TextAlign.center;
+        });
+        break;
+      default:
+    }
+  }
+
+  removeText(BuildContext context, index) {
+    setState(() {
+      info.removeAt(index);
+    });
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text(
+      'Deleted',
+      style: TextStyle(fontSize: 16.0),
+    )));
   }
 }
